306962b4f41688d7450180e22bd8d058d348b2c9
Don Smith <don@antiantonym.com>
2017-07-24 12:13:14 +1200

    add code samples from lecture

--- client/index.js
+++ client/index.js
@@ -1,14 +1,31 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-function helloTemplate (props) {
+function HelloTemplate (props) {
   return (
-    <div>hello {props.name}</div>
+    <div>
+      <div className='red'>hello {props.name}</div>
+      <br />
+      <GoodByeTemplate
+        name='Don'
+        msg={props.name}
+        complex={{a:1, b:2}} />
+    </div>
+  )
+}
+
+function GoodByeTemplate (props) {
+  return (
+    <div>
+      <div>good bye {props.name}</div>
+      <div>{props.msg}</div>
+      <div>{props.complex.a}</div>
+    </div>
   )
 }
 
 var data = { name: 'mix' }
-var view = helloTemplate(data)
+var view = HelloTemplate(data)
 
 var placeToMount = document.getElementById('root')
 
