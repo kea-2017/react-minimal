2bccff6e121343c9da571d524a28286998a66ee4
Don Smith <don@antiantonym.com>
2017-07-24 15:11:50 +1200

    add map example

--- client/index.js
+++ client/index.js
@@ -1,16 +1,27 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-function helloTemplate (props) {
+function ColorList (props) {
   return (
-    <div>hello {props.name}</div>
+    <div>
+    {props.list.map((color, id) => (
+      <Color key={id} color={color} />
+      )
+    )}
+    </div>
   )
 }
 
-var data = { name: 'mix' }
-var view = helloTemplate(data)
+function Color (props) {
+  return (
+    <div style={{backgroundColor: props.color, height: '20px', width: '20px'}} />
+  )
+}
+
+var data = {}
+data.list = ['blue', 'green', 'red']
+var view = ColorList(data)
 
 var placeToMount = document.getElementById('root')
 
 ReactDOM.render(view, placeToMount)
-
